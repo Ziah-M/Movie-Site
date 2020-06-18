0c354e30fe1bb6df213fb39adf6b99163235a92f
Ziah-M <65767717+Ziah-M@users.noreply.github.com>
2020-06-18 13:57:24 +1000

    Basic features

--- Pages/Movies/TrendingMoviesGridOverlay.js
+++ Pages/Movies/TrendingMoviesGridOverlay.js
@@ -1,17 +1,59 @@
 import React from "react";
-import { Carousel, Container, Row, Col, Image } from "react-bootstrap";
+import {
+  Carousel,
+  Container,
+  Row,
+  Col,
+  Image,
+  CardGroup,
+  Card as UnstyledCard
+} from "react-bootstrap";
 import styled from "styled-components";
 
-const TrendingMoviesGridOverlay = () => (
+const TrendingMoviesGridOverlay = ({ movies = [{}] }) => (
   <StyledContainer>
-    <h3>Trending this week</h3>
+    {movies && (
+      <Row
+        noGutters
+        style={{ maxWidth: "25vw", display: "flex", justifyContent: "center" }}
+      >
+        <h3 style={{ textAlign: "center" }}>Trending this week</h3>
+        {movies.map(
+          (movie, index) =>
+            index < 9 && (
+              <Col xs={4}>
+                <MovieCard movie={movie} />
+              </Col>
+            )
+        )}
+      </Row>
+    )}
   </StyledContainer>
 );
 
 const StyledContainer = styled(Container)`
   position: absolute;
-  bottom: 0px;
-  left: 0px;
+  top: 20%;
+  left: 70%;
+`;
+
+const MovieCard = ({ movie: { poster_path, title } }) => {
+  const imgSrc = `https://image.tmdb.org/t/p/original/${poster_path}`;
+  return (
+    <ContainerImg fluid>
+      <StyledCardImg src={imgSrc} alt="" />
+    </ContainerImg>
+  );
+};
+
+const ContainerImg = styled(Container)`
+  margin: 0.3vw 0;
+  padding: 0;
+`;
+
+const StyledCardImg = styled(Image)`
+  height: 9vw;
+  width: 7vw;
 `;
 
 export default TrendingMoviesGridOverlay;
