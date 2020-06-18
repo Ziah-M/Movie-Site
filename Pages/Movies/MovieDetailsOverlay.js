7a53488c480cda2851311d5962b5b81ba331378c
Ziah-M <65767717+Ziah-M@users.noreply.github.com>
2020-06-18 12:48:18 +1000

    Static style

--- Pages/Movies/MovieDetailsOverlay.js
+++ Pages/Movies/MovieDetailsOverlay.js
@@ -1,20 +1,68 @@
 import React from "react";
-import { Carousel, Container, Row, Col, Image } from "react-bootstrap";
+import {
+  Carousel,
+  Container,
+  Row as UnstyledRow,
+  Col,
+  Image
+} from "react-bootstrap";
 import styled from "styled-components";
 
 const MovieDetailsOverlay = () => (
-  <StyledContainer>
-    <h1>Grizzly Man</h1>
-    <span>2004 | Documentary | 1hr 55mins</span>
-    <button>Watch Now</button>
-    <button>Playlist</button>
+  <StyledContainer fluid>
+    <Row noGutters>
+      <Title>Grizzly Man</Title>
+    </Row>
+    <Row noGutters>
+      <Details>2004 | Documentary | 1hr 55mins</Details>
+    </Row>
+    <Row noGutters>
+      <Col xs={12}>
+        <WatchButton>Watch Now</WatchButton>
+        <PlaylistButton>Playlist</PlaylistButton>
+      </Col>
+    </Row>
   </StyledContainer>
 );
 
+const Row = styled(UnstyledRow)`
+  margin: 10px 0;
+`;
+
 const StyledContainer = styled(Container)`
   position: absolute;
-  top: 0px;
-  left: 0px;
+  top: 30%;
+  left: 10%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const Title = styled.h1``;
+
+const Details = styled.div``;
+
+const WatchButton = styled.button`
+  width: 100px;
+  height: 50px;
+  border-radius: 5px;
+  color: white;
+  border: none;
+  margin: 0px 10px;
+  background-color: #24baef;
+  &:hover {
+    background-color: #007bff;
+  }
+`;
+
+const PlaylistButton = styled.button`
+  width: 100px;
+  height: 50px;
+  border-radius: 5px;
+  background-color: transparent;
+  color: white;
+  border: 1px solid white;
+  margin: 0px 10px;
 `;
 
 export default MovieDetailsOverlay;
