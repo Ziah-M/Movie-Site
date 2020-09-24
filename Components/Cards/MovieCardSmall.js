99b929e5dc03e4f5893017c04bfb97eb84302065
Ziah-M <65767717+Ziah-M@users.noreply.github.com>
2020-09-24 15:15:00 +1000

    Fix sliding of cards causing click event
    Click event will now only trigger if mouseUp is within 40px of mouseDown, to prevent unintended clicks while using the sliders with movie cards.

--- Components/Cards/MovieCardSmall.js
+++ Components/Cards/MovieCardSmall.js
@@ -1,6 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router";
 import styled from "styled-components";
+
+const MovieCardSmall = ({
+  url = "",
+  children,
+  className,
+  isTv,
+  isMovie,
+  isPerson,
+  id,
+}) => {
+  const [mouseXStart, setMouseXStart] = useState(null);
+
+  const imageUrl = url
+    ? `https://image.tmdb.org/t/p/original${url}`
+    : // PROFILE PICTURE NOT FOUND IMAGE
+      "https://i.imgur.com/dnj1Cko.png";
+
+  const history = useHistory();
+
+  const handleMouseDown = (event) => {
+    console.log(event);
+    setMouseXStart(event.clientX);
+  };
+
+  const handleMouseUp = (event) => {
+    console.log(event);
+
+    if (
+      event.clientX >= mouseXStart - 40 &&
+      event.clientX <= mouseXStart + 40
+    ) {
+      triggerRouting();
+    }
+  };
+
+  const triggerRouting = () => {
+    if (isMovie) {
+      history.push(`/movieserver/movie/${id}`);
+    }
+
+    if (isTv) {
+      history.push(`/movieserver/tv/details/${id}`);
+    }
+
+    if (isPerson) {
+      history.push(`/movieserver/actor/${id}`);
+    }
+  };
+
+  return (
+    <Wrapper
+      className={className}
+      onMouseDown={(event) => handleMouseDown(event)}
+      onMouseUp={(event) => handleMouseUp(event)}
+    >
+      <Img src={imageUrl} />
+      {children}
+    </Wrapper>
+  );
+};
 
 // POSTER ASPECT RATIO = Height = 1.5 x Width
 
@@ -24,43 +84,5 @@ const Img = styled.img`
   width: 100%;
   height: 100%;
 `;
-
-const MovieCardSmall = ({
-  url = "",
-  children,
-  className,
-  isTv,
-  isMovie,
-  isPerson,
-  id,
-}) => {
-  const imageUrl = url
-    ? `https://image.tmdb.org/t/p/original${url}`
-    : // PROFILE PICTURE NOT FOUND IMAGE
-      "https://i.imgur.com/dnj1Cko.png";
-
-  const history = useHistory();
-
-  const handleClick = () => {
-    if (isMovie) {
-      history.push(`/movieserver/movie/${id}`);
-    }
-
-    if (isTv) {
-      history.push(`/movieserver/tv/details/${id}`);
-    }
-
-    if (isPerson) {
-      history.push(`/movieserver/actor/${id}`);
-    }
-  };
-
-  return (
-    <Wrapper className={className} onClick={() => handleClick()}>
-      <Img src={imageUrl} />
-      {children}
-    </Wrapper>
-  );
-};
 
 export default MovieCardSmall;
